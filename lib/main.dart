cf60f931684c633a7446a6d2215cfe4805429d85
Anis <anis000romzi@gmail.com>
2022-10-26 10:02:49 +0700

    first commit

---
+++ lib/main.dart
@@ -0,0 +1,11 @@
+import 'package:flutter/material.dart';
+import 'Screens/login.dart';
+
+void main() => runApp(const MaterialApp(
+      debugShowCheckedModeBanner: false,
+      home:
+          Login(), 
+          /**  Buat Class Baru yg bernama 
+           * MyScreen di dalam lib bikin folder baru screens isinya 
+           * MyScreen.dart **/
+    ));
